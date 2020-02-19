3e4ec9d60e657201a08c6725ad43ef5fa5e36090
Unity Technologies <@unity.com>
2020-02-20 00:00:00 +0100

    com.unity.xr.mock-hmd@1.0.0-preview.1 # Changelog All notable changes to this package will be documented in this file.
    The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
    and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
    
    ## [1.0.0-preview.1] - 2020-02-20
    
     * Moved NativeConfig to MockHMD class.
     * Added manual and API documentation.
     * Updated dependency: `com.unity.xr.management` 3.0.5 -> 3.0.6
    
    ## [0.1.0-preview.3] - 2020-01-24
    
     * Added ability to choose render mode at build time in Project Settings -> MockHMD.
     * Fixed stereo separation to match legacy Mock HMD.
     * Updated dependency: `com.unity.xr.management` 3.0.1 -> 3.0.5
     * Removed log spam `SinglePass ON / OFF`
    
    ## [0.1.0-preview.2] - 2019-10-29
    
    Fixed missing meta files issue.
    
    ## [0.1.0-preview.1] - 2019-08-02
    
    ### This is the first release of *Starter Kit \<com.unity.xr.starter-kit\>*.
    
    *Short description of this release*

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+# Changelog
+All notable changes to this package will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [1.0.0-preview.1] - 2020-02-20
+
+ * Moved NativeConfig to MockHMD class.
+ * Added manual and API documentation.
+ * Updated dependency: `com.unity.xr.management` 3.0.5 -> 3.0.6
+
+## [0.1.0-preview.3] - 2020-01-24
+
+ * Added ability to choose render mode at build time in Project Settings -> MockHMD.
+ * Fixed stereo separation to match legacy Mock HMD.
+ * Updated dependency: `com.unity.xr.management` 3.0.1 -> 3.0.5
+ * Removed log spam `SinglePass ON / OFF`
+
+## [0.1.0-preview.2] - 2019-10-29
+
+Fixed missing meta files issue.
+
+## [0.1.0-preview.1] - 2019-08-02
+
+### This is the first release of *Starter Kit \<com.unity.xr.starter-kit\>*.
+
+*Short description of this release*
---
+++ Runtime/MockHMDBuildSettings.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Management;
+
+namespace Unity.XR.MockHMD
+{
+    /// <summary>
+    /// Build-time settings for MockHMD provider.
+    /// </summary>
+    [XRConfigurationData("MockHMD", "xr.sdk.mock-hmd.settings")]
+    public class MockHMDBuildSettings : ScriptableObject
+    {
+        /// <summary>
+        /// Stereo rendering mode.
+        /// </summary>
+        public enum RenderMode
+        {
+            /// <summary>
+            /// Submit separate draw calls for each eye.
+            /// </summary>
+            MultiPass,
+
+            /// <summary>
+            /// Submit one draw call for both eyes.
+            /// </summary>
+            SinglePassInstanced,
+        };
+
+        /// <summary>
+        /// Stereo rendering mode.
+        /// </summary>
+        public RenderMode renderMode;
+
+        /// <summary>
+        /// Runtime access to build settings. 
+        /// </summary>
+        public static MockHMDBuildSettings Instance;
+
+        void OnEnable()
+        {
+            Instance = this;
+        }
+    }
+}
