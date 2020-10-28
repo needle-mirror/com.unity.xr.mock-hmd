288eb6bf1f24c889e0bf2e097e239984f8c5b0f4
Unity Technologies <@unity>
2020-10-28 00:00:00 +0000

    com.unity.xr.mock-hmd@1.2.0-preview.1 ## [1.2.0-preview.1] - 2020-10-28
     * Added public API for setting the resolution of the eye textures.
     * Added public API for setting the mirror view crop amount.
     * Mirror view now takes render viewport scale into account.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.2.0-preview.1] - 2020-10-28
+
+ * Added public API for setting the resolution of the eye textures.
+ * Added public API for setting the mirror view crop amount.
+ * Mirror view now takes render viewport scale into account.
+
 ## [1.1.3-preview.1] - 2020-08-31
 
  * Win32 x86 player support.
--- Runtime/MockHMD.cs
+++ Runtime/MockHMD.cs
@@ -16,5 +16,19 @@ namespace Unity.XR.MockHMD
         /// <returns>true if render mode successfully set</returns>
         [DllImport(LibraryName, EntryPoint = "NativeConfig_SetRenderMode")]
         public static extern bool SetRenderMode(MockHMDBuildSettings.RenderMode renderMode);
+
+        /// <summary>
+        /// Set the resolution of the eye textures.
+        /// </summary>
+        [DllImport(LibraryName, EntryPoint = "NativeConfig_SetEyeResolution")]
+        public static extern bool SetEyeResolution(int width, int height);
+
+        /// <summary>
+        /// Set the crop value applied when rendering the mirror view.
+        /// This is useful to remove the peripheral distorted part of the image.
+        /// </summary>
+        /// <param name="crop">the amount to remove from the image, valid range is 0.0 to 0.5</param>
+        [DllImport(LibraryName, EntryPoint = "NativeConfig_SetMirrorViewCrop")]
+        public static extern bool SetMirrorViewCrop(float crop);
     }
 }
